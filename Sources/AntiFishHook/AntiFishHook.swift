b9f1b5a44a1f10a168e2864372ff1315ff999279
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-02-22 12:44:14 +0900

    implemented anti fishhook

--- Sources/AntiFishHook/AntiFishHook.swift
+++ Sources/AntiFishHook/AntiFishHook.swift
@@ -1,2 +1,95 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import MachOKit
+import FishHook
+
+public enum AntiFishHook {
+    @inline(__always)
+    static public func denyFishHook(_ symbol: String) {
+        var symbolAddress: UnsafeRawPointer?
+        for image in MachOImage.images {
+            if symbolAddress == nil {
+                symbolAddress = find(symbol, in: image)
+            }
+            if let symbolAddress {
+                FishHook.rebind_symbols_image(
+                    machO: image,
+                    rebindings: [
+                        .init(
+                            name: symbol,
+                            replacement: .init(mutating: symbolAddress),
+                            replaced: nil
+                        )
+                    ]
+                )
+            }
+        }
+    }
+
+    @inline(__always)
+    @discardableResult
+    static public func denyFishHook(_ symbol: String, at image: MachOImage) -> Bool {
+        guard let symbolAddress = find(symbol, in: image) else {
+            return false
+        }
+        FishHook.rebind_symbols_image(
+            machO: image,
+            rebindings: [
+                .init(
+                    name: symbol,
+                    replacement: .init(mutating: symbolAddress),
+                    replaced: nil
+                )
+            ]
+        )
+        return true
+    }
+}
+
+extension AntiFishHook {
+    private static func find(_ symbol: String, in machO: MachOImage) -> UnsafeRawPointer? {
+        var libraryOrdinal: Int?
+
+        if let symbol = machO.bindingSymbols.first(where: {
+            $0.symbolName == "_" + symbol
+        }) { libraryOrdinal = symbol.libraryOrdinal }
+        else if let symbol = machO.lazyBindingSymbols.first(where: {
+            $0.symbolName == "_" + symbol
+        }) { libraryOrdinal = symbol.libraryOrdinal }
+        else if let symbol = machO.weakBindingSymbols.first(where: {
+            $0.symbolName == "_" + symbol
+        }) { libraryOrdinal = symbol.libraryOrdinal }
+
+        else if let dyldChainedFixups = machO.dyldChainedFixups,
+           let `import` = dyldChainedFixups.imports.first(where: {
+               dyldChainedFixups.symbolName(for: $0.info.nameOffset) == "_" + symbol
+           }) {
+            libraryOrdinal = `import`.info.libraryOrdinal
+        }
+
+        guard let libraryOrdinal else { return nil }
+        var targetMachO: MachOImage?
+
+        if libraryOrdinal == 0 {
+            targetMachO = machO
+        } else {
+            let libraryName = machO.dependencies[libraryOrdinal - 1].dylib.name
+                .components(separatedBy: "/")
+                .last!
+                .components(separatedBy: ".")
+                .first!
+            targetMachO = MachOImage(name: libraryName)
+        }
+
+        guard let targetMachO else { return nil }
+
+        return _findExportedSymbol(symbol, in: targetMachO)
+    }
+
+    private static func _findExportedSymbol(_ symbol: String, in machO: MachOImage) -> UnsafeRawPointer? {
+        let exportedSymbols = machO.exportedSymbols
+        guard let exportedSymbol = exportedSymbols.first(where: {
+            $0.name == "_" + symbol
+        }) else { return nil }
+        return machO.ptr.advanced(by: exportedSymbol.offset)
+    }
+}
