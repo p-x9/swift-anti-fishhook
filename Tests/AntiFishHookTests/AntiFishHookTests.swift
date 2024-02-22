d41f44abf65ea2f32246d9598cc6e43ccc2d67d2
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-02-22 12:46:20 +0900

    add test cases

--- Tests/AntiFishHookTests/AntiFishHookTests.swift
+++ Tests/AntiFishHookTests/AntiFishHookTests.swift
@@ -1,4 +1,45 @@
 import XCTest
 @testable import AntiFishHook
+import MachOKit
+import FishHook
 
-final class AntiFishHookTests: XCTestCase {}
+final class AntiFishHookTests: XCTestCase {
+    func test() {
+        let item = StructItem()
+
+        guard let machO = MachOImage(name: "AntiFishHookTests") else { return }
+        guard let to = machO.symbol(
+            named: "$s17AntiFishHookTests10StructItemV11replacementSSyF"
+        ) else {
+            return
+        }
+
+        /* Origianl */
+        XCTAssertEqual(item.target(), "target")
+        XCTAssertEqual(item.replacement(), "replacement")
+
+        let targetSymbolName = "$s17AntiFishHookTests10StructItemV6targetSSyF"
+        let rebindings: [Rebinding] = [
+            .init(
+                name: targetSymbolName,
+                replacement: .init(mutating: machO.ptr.advanced(by: to.offset)),
+                replaced: nil
+            )
+        ]
+
+        /* Fish Hook */
+        FishHook.rebind_symbols_image(
+            machO: machO,
+            rebindings: rebindings
+        )
+
+        XCTAssertEqual(item.target(), "replacement")
+        XCTAssertEqual(item.replacement(), "replacement")
+
+        /* Deny Fish Hook */
+        AntiFishHook.denyFishHook(targetSymbolName, at: machO)
+
+        XCTAssertEqual(item.target(), "target")
+        XCTAssertEqual(item.replacement(), "replacement")
+    }
+}
---
+++ Tests/AntiFishHookTests/struct.swift
@@ -0,0 +1,19 @@
+//
+//  struct.swift
+//
+//
+//  Created by p-x9 on 2024/02/22.
+//  
+//
+
+import Foundation
+
+struct StructItem {
+    func target() -> String {
+        "target"
+    }
+
+    func replacement() -> String {
+        "replacement"
+    }
+}
